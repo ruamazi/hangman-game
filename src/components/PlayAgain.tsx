a94b7a35933435c8e92fe83eca9996ce50fc9118
Mohssin <barcalover016@gmail.com>
2023-11-03 22:01:39 +0100

    temp fix

--- src/components/PlayAgain.tsx
+++ src/components/PlayAgain.tsx
@@ -1,13 +1,15 @@
 import { Dispatch, SetStateAction } from "react";
+import words from "../wordList.json";
 
 type SetW = {
   setWordToGuess: Dispatch<SetStateAction<string>>;
 };
 
 const PlayAgain = ({ setWordToGuess }: SetW) => {
+  const randomWord = Math.floor(Math.random() * words.length);
   const refresh = () => {
     window.location.reload();
-    setWordToGuess((prev) => prev);
+    setWordToGuess(words[randomWord]);
   };
   return (
     <button onClick={refresh} className="play-again">
