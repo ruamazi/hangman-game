acf7a384a31548924c5709f2f09d4960f88316a7
Mohssin <barcalover016@gmail.com>
2023-11-03 20:53:31 +0100

    adding the word to console log

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,9 @@ import PlayAgain from "./components/PlayAgain";
 
 function App() {
   const randomWord = Math.floor(Math.random() * words.length);
-  const [wordToGuess, setWordToGuess] = useState<string>(words[randomWord]);
+  const wordToGuess: string = words[randomWord];
+  // const [wordToGuess, setWordToGuess] = useState<string>();
+  console.log(wordToGuess);
 
   const [gussedLetters, setGuessedLetters] = useState<string[]>([]);
 
